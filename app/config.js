f19823203055b177f0de72f6db9adc80cae70c3f
farrukhishaq <farrukh.ishaq@digital.homeoffice.gov.uk>
2023-03-27 17:27:02 +0100

    implemented typeahead.

--- app/config.js
+++ app/config.js
@@ -7,7 +7,7 @@ module.exports = {
     serviceName: 'Service name goes here',
 
     // Default port that prototype runs on
-    port: '3000',
+    port: '4000',
 
     // Enable or disable password protection on production
     useAuth: 'true',
